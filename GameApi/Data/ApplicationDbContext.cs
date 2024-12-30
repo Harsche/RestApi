748cb5310bf66c217adc0af7a839074cefdf0761
Harsche <henrique_harsche@hotmail.com>
2024-12-30 05:02:45 -0300

    Add first migration

--- GameApi/Data/ApplicationDbContext.cs
+++ GameApi/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using System;
+using GameApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameApi.Data;
@@ -6,5 +7,16 @@ namespace GameApi.Data;
 public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User{Id= 1, Username="Ocoloso", CreateDate = new DateTime(2024, 12, 30)},
+            new User{Id= 2, Username="Biloso", CreateDate = new DateTime(2024, 9, 11)},
+            new User{Id= 3, Username="Ocoscocosco", CreateDate = new DateTime(2024, 1, 11)}
+        );
+    }
 
 }
---
+++ GameApi/Migrations/20241230080101_AddUserTableAndSeedData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GameApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTableAndSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreateDate", "Username" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 12, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ocoloso" },
+                    { 2, new DateTime(2024, 9, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Biloso" },
+                    { 3, new DateTime(2024, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ocoscocosco" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
