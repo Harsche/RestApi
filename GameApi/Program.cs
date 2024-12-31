7e59b5088f5a5ab8cbae1402bc7e1b8c4445cc66
Harsche <henrique_harsche@hotmail.com>
2024-12-30 22:48:17 -0300

    Add new packages to support patch feature

--- GameApi/Program.cs
+++ GameApi/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 
 var connectionString = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddSqlServer<ApplicationDbContext>(connectionString);
